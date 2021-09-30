c51f344cc6a21890311271f36ec564f8ecd730a9
yooshmaa <yooshioo.m@gmail.com>
2021-09-30 10:30:59 +0900

    commit git_study_2.php for the first time

---
+++ git_study_2.php
@@ -0,0 +1,11 @@
+<?php
+$fruits = array(
+	'apple' => 'リンゴ',
+	'orage' => 'オレンジ',
+	'grapes' => 'ブドウ',
+);
+
+foreach ($fruits as $key => $value) {
+	echo $key . ' is ' . $value . ' in Japanese.' . '<br>';
+}
+?>
