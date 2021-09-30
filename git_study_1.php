3f6ac6fe2104d2881818b6a16d0a94e1cd073f87
yooshmaa <yooshioo.m@gmail.com>
2021-09-30 10:13:44 +0900

    first commit

---
+++ git_study_1.php
@@ -0,0 +1,12 @@
+<?php
+$number1 = 10;
+$number2 = 20;
+
+if ($number1 - $number2 > 0) {
+	echo 'number1 is bigger than number2.';
+} elseif ($number2 - $number1 > 0) {
+	echo 'number2 is bigger than number1.';
+} else {
+	echo 'both numbers are equal.';
+}
+?>
