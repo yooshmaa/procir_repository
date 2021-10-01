039f1a4ce727ca0325f9276d341dca77c13f12d6
yooshmaa <yooshioo.m@gmail.com>
2021-10-01 09:29:05 +0900

    git_study_1.php is modified

--- git_study_1.php
+++ git_study_1.php
@@ -1,7 +1,15 @@
 <?php
-$number1 = 10;
-$number2 = 5;
+$numbers = array(
+	'number1' => 10,
+	'number2' => 5,
+	'number3' => 7.2,
+);
 
-$total = $number1 + $number2;
-echo 'total amount is ' . $total;
+foreach ($numbers as $number) {
+	if (is_int($number)) {
+		echo $number . ' is integer.' . '<br>';
+	} else {
+		echo $number . ' is not integer.' . '<br>';
+	}
+}
 ?>
